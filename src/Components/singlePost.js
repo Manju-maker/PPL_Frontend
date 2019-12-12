4f969b0877b42455a820cd966b20bbff184acf70
Manju <manju.bhatt@daffodilsw.com>
2019-12-12 17:41:19 +0530

    final commit

--- src/Components/singlePost.js
+++ src/Components/singlePost.js
@@ -295,51 +295,6 @@ class SinglePost extends React.Component {
                             </div>
                             <div className="contnt_3">
                                 <ul>
-                                    <li>
-                                        <div className="list_image">
-                                            <div className="image_sec">
-                                                <img src="/images/post_img.png" />
-                                            </div>
-                                            <div className="image_name">
-                                                Bharat
-                                            </div>
-                                        </div>
-                                        <div className="list_info">
-                                            This is an example of a comment. You
-                                            can create as many comments like
-                                            this one or sub comments as you like
-                                            and manage all of your content
-                                            inside your Account.
-                                        </div>
-                                        <input
-                                            type="button"
-                                            defaultValue="Reply"
-                                            className="orng_btn"
-                                        />
-                                    </li>
-
-                                    <li>
-                                        <div className="list_image">
-                                            <div className="image_sec">
-                                                <img src="/images/post_img.png" />
-                                            </div>
-                                            <div className="image_name">
-                                                Bharat
-                                            </div>
-                                        </div>
-                                        <div className="list_info">
-                                            This is an example of a comment. You
-                                            can create as many comments like
-                                            this one or sub comments as you like
-                                            and manage all of your content
-                                            inside your Account.
-                                        </div>
-                                        <input
-                                            type="button"
-                                            defaultValue="Reply"
-                                            className="orng_btn"
-                                        />
-                                    </li>
                                     <div>
                                         {this.state.pictureComment.length > 0
                                             ? this.state.pictureComment.map(
